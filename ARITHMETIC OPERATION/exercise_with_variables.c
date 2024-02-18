1da9c5c3c5d972af9f694b9d37205e4cca7de69c
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-18 11:58:57 +0100

    Create exercise_with_variables.c

---
+++ ARITHMETIC OPERATION/exercise_with_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b, c;
+    a = 3 + 2;
+    b = 2 * 5;
+    c = 2;
+
+    printf("%f\n", pow((double)(a) / (b), c));
+    return 0;
+}
